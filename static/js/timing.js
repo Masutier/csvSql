319578a2436d2cc3858c66c785fcf092a0c63b4e
Masutier <masutier@gmail.com>
2023-02-26 21:55:23 -0500

    delete and modify some templates

--- static/js/timing.js
+++
@@ -1,6 +0,0 @@
-var message_ele = document.getElementById("message_container");
-    if (message_ele != null) {
-        setTimeout(function() {
-            message_ele.style.display = "none";
-        }, 4000);
-    }
