73aaa41911770f9c830d0c9412928277ad4c6a02
Masutier <masutier@gmail.com>
2023-03-06 23:41:40 -0500

    modifications in csvPandas and csvSqlFull witch is not working very good

--- csvPandas.py
+++ csvPandas.py
@@ -2,29 +2,37 @@ import numpy as np
 import pandas as pd
 import sqlite3 as sql
 
-df = pd.read_csv("/home/gabriel/prog/data_samples/100Records.csv")
+
+covid_big = [
+    "covid_big_1.csv", "covid_big_2.csv", "covid_big_3.csv", "covid_big_4.csv",
+    "covid_big_5.csv", "covid_big_6.csv", "covid_big_7.csv", "covid_big_8.csv"
+]
 
 
 # Crear la Base de Datos
 def createDB():
-    conn=sql.connect("/home/gabriel/prog/analytics/csvSql/dbs/testdbpan.db")
+    conn=sql.connect("/home/gabriel/prog/csvSql/dbs/covidCases.db")
     conn.commit()
     conn.close()
 
 
-# Limpiar los nombres de las columnas
-def cleanColumns():
-    df.columns = [x.upper().replace(" ","_").replace("-","_").replace("$","").replace("?","").replace("%","").replace(".","") \
-                .replace("@","").replace("#","").replace(r"/","").replace("\\","").replace(r"(","").replace(")","") for x in df.columns]
-
-
 # Insertar el csv a la Base de Datos
 def insertData():
-    conn=sql.connect("/home/gabriel/prog/analytics/csvSql/dbs/testdbpan.db")
-    df.to_sql(name='hundred', con=conn, if_exists="append", index=False)
-    conn.close()
+
+    for covid in covid_big:
+        df = pd.read_csv('/home/gabriel/prog/csvSql/docs/' + covid)
+
+        df.columns = [x.upper().replace(" ","_").replace("-","_").replace("$","").replace("?","").replace("%","").replace(".","") \
+            .replace("@","").replace("#","").replace(r"/","").replace("\\","").replace(r"(","").replace(")","") for x in df.columns]
+
+        conn=sql.connect("/home/gabriel/prog/csvSql/dbs/covidCases.db")
+        df.to_sql(name="covid19", con=conn, if_exists="append", index=False)
+        conn.close()
 
 
-#createDB()
-#cleanColumns()
-#insertData()
+"""
+createDB()
+"""
+insertData()
+print("Full load")
+
--- csvSqlFull.py
+++ csvSqlFull.py
@@ -1,7 +1,28 @@
-import sqlite3
+import csv
+import sqlite3 as sql
+import pandas as pd
+
+
+
+# DIVIDE BIG CSV FILES
+def splitCsv():
+    chunk_size = 900000
+    batchNum = 1
+
+    for chunk in pd.read_csv("/home/gabriel/prog/csvSql/docs/Casos_positivos_de_COVID-19_en_Colombia.csv", chunksize=chunk_size):
+        chunk.to_csv("/home/gabriel/prog/csvSql/docs/covid_big_" + str(batchNum) + ".csv", index=False)
+        batchNum += 1
+
+
+# Crea la db
+def createDB():
+    conn=sql.connect('/home/gabriel/prog/csvSql/dbs/covidCases.db')
+    conn.commit()
+    conn.close()
+
 
 def csv_sql(file_dir, table_name, database_name):
-    conn = sqlite3.connect(database_name)
+    conn = sql.connect(database_name)
     cur = conn.cursor()
     # Drop the current table by: 
     cur.execute("DROP TABLE IF EXISTS %s;" % table_name)
@@ -19,5 +40,16 @@ def csv_sql(file_dir, table_name, database_name):
 
 
 
-# Example:
-csv_sql('/home/gabriel/prog/data_samples/100Records.csv', 'hundred', '/home/gabriel/prog/analytics/csvSql/dbs/testdbsample.db')
+#splitCsv()
+#createDB()
+#csv_sql('/home/gabriel/prog/csvSql/docs/Casos_positivos_de_COVID-19_en_Colombia.csv', 'covid', '/home/gabriel/prog/csvSql/dbs/covidCases.db')
+covid_big = [
+    "/covid_big_1.csv", "/covid_big_2.csv", "/covid_big_3.csv", "/covid_big_4.csv", "/covid_big_5.csv",
+    "/covid_big_6.csv", "/covid_big_7.csv", "/covid_big_8.csv"
+    ]
+
+for covid in covid_big:
+    print(covid)
+    csv_sql('/home/gabriel/prog/csvSql/docs/covid_big_1.csv', 'covid', '/home/gabriel/prog/csvSql/dbs/covidCases.db')
+
+print("Full load")
